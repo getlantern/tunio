b149984c76b2ad87ddb73641ff5ae6d200db85cf
José Carlos Nieto <jose.carlos@menteslibres.net>
2015-11-27 03:50:48 +0000

    Making sure the tunio command is still working after all those changes.

--- cmd/tunio/main.go
+++ cmd/tunio/main.go
@@ -9,17 +9,30 @@ import (
 var (
 	deviceName = flag.String("tundev", "tun0", "TUN device name.")
 	deviceIP   = flag.String("netif-ipaddr", "", "Address of the virtual router inside the TUN device.")
-	deviceMask = flag.String("netif-netmask", "", "Network mask that defines the traffic that is going to be redirected to the TUN device.")
-	proxyAddr  = flag.String("proxy-addr", "", "Lantern address.")
-	udpgwAddr  = flag.String("udpgw-remote-server-addr", "", "UDPGW remote server address (optional).")
+	deviceMask = flag.String("netif-netmask", "255.255.255.0", "Network mask that defines the traffic that is going to be redirected to the TUN device.")
+	proxyAddr  = flag.String("proxy-addr", "", "Lantern proxy address.")
+	udpgwAddr  = flag.String("udpgw-remote-server-addr", "", "udpgw remote server address (optional).")
 )
 
 func main() {
 	flag.Parse()
 
-	dialer := tunio.NewLanternDialer(*proxyAddr)
+	if *deviceName == "" {
+		log.Fatal("missing tundev")
+	}
+
+	if *deviceIP == "" {
+		log.Fatal("missing netif-ipaddr")
+	}
+
+	if *proxyAddr == "" {
+		log.Fatal("missing proxy-addr")
+	}
 
 	log.Printf("Configuring device %q (ipaddr: %q, netmask: %q)", *deviceName, *deviceIP, *deviceMask)
+
+	dialer := tunio.NewLanternDialer(*proxyAddr, nil)
+
 	if err := tunio.ConfigureTUN(*deviceName, *deviceIP, *deviceMask, *udpgwAddr, dialer); err != nil {
 		log.Fatalf("Failed to configure device: %q", err)
 	}
